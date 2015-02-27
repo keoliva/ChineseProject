df44f510320c7c34cfe90b9407a2e3456273f924
keoliva <keoliva@andrew.cmu.edu>
2015-02-27 03:07:15 -0500

    logged in user redirects to new page
    this new page includes basic fields that require Chinese/English/Pinyin.
    Also, basic values such as user,login_url,logout_url is stored in a
    global variable

--- chinesethes/main.py
+++ chinesethes/main.py
@@ -5,22 +5,42 @@ import logging
 from google.appengine.api import users
 from google.appengine.ext import ndb
 
-class Character(ndb.Model):
-
 jinja_environment = jinja2.Environment(loader = jinja2.FileSystemLoader(os.path.dirname(__file__)))
                                        
+class Struct(): pass
+data = Struct()
+data.main_info = {'user': users.get_current_user(),
+        'login_url': users.create_login_url('/'),
+        'logout_url': users.create_logout_url('/')}
 
 class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        if (data.main_info['user']):
+            self.redirect('/enter')
+        
+        template_values = {}
+        template_values.update(data.main_info)
+        
+        template = jinja_environment.get_template('home.html')
+        self.response.write(template.render(template_values))
+
+class EnterWordsHandler(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         login_url = users.create_login_url('/')
         logout_url = users.create_logout_url('/')
-        
-        template_values = {'user':user, 'login_url':login_url,
-        'logout_url':logout_url}
-        template = jinja_environment.get_template('home.html')
-        self.response.write(template.render(template_values)
+
+        template_values = {}
+        template_values.update(data.main_info)
+
+        template = jinja_environment.get_template('enter.html')
+        self.response.write(template.render(template_values))
+    def post(self):
+        chi = self.request.get("chi")
+        eng = self.request.get("eng")
+        pinyin = self.request.get("pinyin")
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/enter', EnterWordsHandler),
 ], debug=True)
