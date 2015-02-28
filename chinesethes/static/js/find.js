aa9c85f0d9caf25f9170d4be75d643a4ba5ab71e
keoliva <keoliva@andrew.cmu.edu>
2015-02-28 18:29:05 -0500

    have access to character chosen in grid

--- chinesethes/static/js/find.js
+++ chinesethes/static/js/find.js
@@ -1,12 +1,16 @@
-setInterval(function find() {
-	var text = document.getElementById("textarea").value;
-	var index = text.search("#");
-	if (index >= 0) {
-	  var n_text = text.slice(index);
-      var len_of_word = n_text.search(" ");
-      document.getElementById("demo").innerHTML = 
-				text.substr(index+1, len_of_word); 
-	} else {
-		document.getElementById("demo").innerHTML = "dom";
-	}
-}, 2000);
+$(function () {
+	$("textarea").keypress(function() {
+		var text = $("#textarea").val();
+		var index = text.search("#");
+		if (index >= 0) {
+			var n_text = text.slice(index);
+			var len_of_word = n_text.search(" ");
+			$("#demo").html(text.substr(index+1, len_of_word)); 
+		} else {
+			$("#demo").html("dom");
+		}
+	}),
+	$("table td p").mousedown(function() {
+		alert($(this).html());
+	})
+});
