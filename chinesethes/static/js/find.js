decc2dbec99e2e6b21774702a73ffbea021a52b0
keoliva <keoliva@andrew.cmu.edu>
2015-03-01 06:47:26 -0500

    updated unordered list with words /\b#/
    /\b#/ is JavaScript regexp for strings with '#' at either beginning or
    end of string

--- chinesethes/static/js/find.js
+++ chinesethes/static/js/find.js
@@ -4,5 +4,26 @@ $(function () {
 	}),
 	$("#words_to_find li").click(function() {
 		alert("yomama");
+	}),
+	$('#sync_button').click(function() {
+		var text = document.getElementById("textarea").value;
+		var words = text.split(" ");
+		var i = 0, word = "", current_html = "", new_html = "";
+		$('#words_to_find').html("");
+		for (i = 0; i < words.length; i++) {
+			if (words[i][0] == '#') {
+				word = words[i].slice(1);
+				id = word;
+				word = word.replace(/_/g, " ");
+				current_html = $('#words_to_find').html();
+				new_html = "<li id=" + id + ">" + word + "</li>";
+				$('#words_to_find').html(current_html + new_html);
+			}
+		}
+	}),
+	$('#textarea').keydown(function(event) {
+		if (event.keyCode == 118) {
+			$('#sync_button').click();
+		}
 	})
 });
